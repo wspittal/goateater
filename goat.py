2c67db5696e28c215c9a48ca0197f57627475058
wspittal <114256362+wspittal@users.noreply.github.com>
2022-09-23 14:45:29 -0400

    Create goat.py

---
+++ goat.py
@@ -0,0 +1,13 @@
+import turtle, time
+
+background = turtle.Screen()
+background.setup(500, 500)
+background.bgpic("goat2.gif")
+
+print("this is a goat")
+time.sleep(5)
+print("I like this goat")
+time.sleep(3)
+print("This goat is my dinner")
+time.sleep(1)
+background.bgpic("eatgoat.gif")
